1a6809004828b5d3699c9980d8a550ede491b1b1
avil3 <alexisrviltres@yahoo.com>
2023-04-13 23:28:00 +0000

    Miles per gallon
    New commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #your code here!
-
+miles_driven = float(input())
+gallons_used = float(input())
 #read in the miles and read in the gallons used.
-
+miles_per_gallon = miles_driven / gallons_used
 #display the miles per gallon
+print(miles_per_gallon)
